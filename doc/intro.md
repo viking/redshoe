2406f7a09a24bdd786b4a994e19743b1e9e444fc
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2016-08-03 09:59:21 -0500

    Generate application with lein

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to redshoe
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/redshoe/core_test.clj
@@ -0,0 +1,7 @@
+(ns redshoe.core-test
+  (:require [clojure.test :refer :all]
+            [redshoe.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
