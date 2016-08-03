bde7497b55567b5dc35dd6c859262466c5845e4e
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2016-08-03 16:53:52 -0500

    Rename dictionary namespace to records

---
+++ src/redshoe/records.clj
@@ -0,0 +1,20 @@
+(ns redshoe.records)
+
+(defn- process-item-content
+  [{ tag :tag content :content }]
+  (when content
+    [tag (first content)]))
+
+(defn- process-item
+  [{ tag :tag content :content }]
+  (when (= :item tag)
+    (into {} (map process-item-content content))))
+
+(defn- process-records
+  [{ tag :tag content :content }]
+  (when (= :records tag)
+    (map process-item content)))
+
+(defn from-xml
+  [doc]
+  (process-records doc))
