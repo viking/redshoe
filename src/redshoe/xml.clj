c1fe40afe1493d645e29f93ed8cfbd2e2bbc2690
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2016-08-11 15:06:43 -0500

    Add xml/seq->tags function

--- src/redshoe/xml.clj
+++ src/redshoe/xml.clj
@@ -1,20 +1,46 @@
 (ns redshoe.xml)
 
-(defn- process-item-content
+(defn- process-field-tag
   [{ tag :tag content :content }]
   (when content
     [tag (first content)]))
 
-(defn- process-item
+(defn- process-item-tag
   [{ tag :tag content :content }]
   (when (= :item tag)
-    (into {} (map process-item-content content))))
+    (into {} (map process-field-tag content))))
 
-(defn- process-records
+(defn- process-records-tag
   [{ tag :tag content :content }]
   (when (= :records tag)
-    (map process-item content)))
+    (map process-item-tag content)))
 
 (defn tags->seq
   [doc]
-  (process-records doc))
+  (process-records-tag doc))
+
+(defn- process-field-pairs
+  [[field-name value]]
+  {
+   :tag (keyword field-name)
+   :attrs nil
+   :content [value] })
+
+(defn- process-item-map
+  [m]
+  {
+   :tag :item
+   :attrs nil
+   :content (mapv process-field-pairs m) })
+
+(defn- process-records-seq
+  [s]
+  {
+   :tag :records
+   :attrs nil
+   :content (mapv process-item-map s) })
+
+(defn seq->tags
+  [s]
+  (process-records-seq s))
+
