80b27506306dc8c802326ae32899c2d709853c4f
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2016-08-26 14:30:13 -0500

    Refactor util/export-tree-arms, add export field names

--- src/redshoe/util.clj
+++ src/redshoe/util.clj
@@ -11,37 +11,48 @@
 (def export-records (comp xml/records->seq http/export-records))
 (def export-field-names (comp xml/fields->seq http/export-field-names))
 
-(defn- organize-event
-  [field-groups mapping-groups event]
+(defn- add-exports-to-field
+  [field-name-groups field]
+  (assoc field :exports (get field-name-groups (:field_name field))))
+
+(defn- organize-fields
+  [fields field-name-groups]
+  (map (partial add-exports-to-field field-name-groups) fields))
+
+(defn- add-forms-to-event
+  [mapping-groups field-groups event]
   (->>
     (get mapping-groups (:unique_event_name event))
     (map :form)
     (select-keys field-groups)
     (assoc event :forms)))
 
-(defn- organize-arm
-  [field-groups mapping-groups event-groups arm]
-  (->>
-    (get event-groups (:arm_num arm))
-    (map (partial organize-event field-groups mapping-groups))
-    (assoc arm :events)))
+(defn- organize-events
+  [events mapping-groups field-groups]
+  (map (partial add-forms-to-event mapping-groups field-groups) events))
+
+(defn- add-events-to-arm
+  [event-groups arm]
+  (assoc arm :events (get event-groups (:arm_num arm))))
 
 (defn- organize-arms
-  [field-groups mapping-groups event-groups arms]
-  (map (partial organize-arm field-groups mapping-groups event-groups) arms))
+  [arms event-groups]
+  (map (partial add-events-to-arm event-groups) arms))
 
 (defn export-arms-tree
   "Fetch field metadata and organize by arm and event"
   [url token]
-  (let [fields (export-dictionary url token)
+  (let [field-names (export-field-names url token)
+        fields (export-dictionary url token)
         mappings (export-mappings url token)
         events (export-events url token)
         arms (export-arms url token)]
-    (organize-arms
-      (group-by :form_name fields)
-      (group-by :unique_event_name mappings)
-      (group-by :arm_num events)
-      arms)))
+    (->>
+      (organize-fields fields (group-by :original_field_name field-names))
+      (group-by :form_name)
+      (organize-events events (group-by :unique_event_name mappings))
+      (group-by :arm_num)
+      (organize-arms arms))))
 
 (defn export-chunked-records
   "Fetch records from the REDCap API in chunks to mitigate REDCap API timeout
