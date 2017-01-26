d94cc8b343a4da7f05b486547a6d3feed23ddedf
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2017-01-26 15:44:07 -0600

    Create redshoe.field namespace, add process-field function

---
+++ src/redshoe/field.clj
@@ -0,0 +1,121 @@
+(ns redshoe.field
+  (:require [clojure.string :as string]
+            clj-time.format))
+
+; Conversion helpers
+(defn- str->datetime
+  [fmt-str value]
+  (->
+    (clj-time.format/formatter fmt-str)
+    (clj-time.format/parse value)))
+
+(defn- str->boolean
+  [value]
+  (= value "y"))
+
+(defn- str->number
+  [value]
+  (bigdec value))
+
+(defn- str->integer
+  [value]
+  (Integer. value))
+
+; Table for converting min/max validation metadata
+(def ^{:private true} validation-converters
+  (let [convert-date (partial str->datetime "yyyy-MM-dd")
+        convert-datetime (partial str->datetime "yyyy-MM-dd HH:mm")
+        convert-datetime-s (partial str->datetime "yyyy-MM-dd HH:mm:ss")]
+  {"date_ymd" convert-date
+   "date_mdy" convert-date
+   "date_dmy" convert-date
+   "time" (partial convert-datetime "HH:mm")
+   "datetime_ymd" convert-datetime
+   "datetime_mdy" convert-datetime
+   "datetime_dmy" convert-datetime
+   "datetime_seconds_ymd" convert-datetime-s
+   "datetime_seconds_mdy" convert-datetime-s
+   "datetime_seconds_dmy" convert-datetime-s
+   "number" str->number
+   "integer" str->integer}))
+
+(defn- process-validation
+  [field]
+  (let
+    [{field-type :type
+      validation-type :text_validation_type_or_show_slider_number
+      validation-min :text_validation_min
+      validation-max :text_validation_max
+      choices :select_choices_or_calculations} field
+    field (dissoc
+            field
+            :text_validation_type_or_show_slider_number
+            :text_validation_min
+            :text_validation_max
+            :select_choices_or_calculations)]
+
+    (case field-type
+      "text"
+      (let [converter (get validation-converters validation-type)]
+        ; Convert min and max values from strings to the appropriate type
+        (->>
+          (filter second
+                  {:type validation-type
+                   :min (and converter validation-min (converter validation-min))
+                   :max (and converter validation-max (converter validation-max))
+                   })
+          (into {})
+          (assoc field :validation)))
+
+      "slider"
+      (->>
+        (filter second
+                {:show_slider_number validation-type
+                 :slider_labels (string/split choices #"\s+\|\s+")})
+        (into {})
+        (merge field))
+
+      ("dropdown" "radio" "checkbox")
+      (->>
+        (string/split choices #"\s*\|\s*")
+        (mapv #(zipmap [:code :label] (string/split % #",\s+")))
+        (assoc field :choices))
+
+      "calc"
+      (assoc field :formula choices)
+
+      ; default
+      field)))
+
+; Table for renaming field keys
+(def ^{:private true} field-key-changes
+  {:field_type :type
+   :field_name :name
+   :field_label :label})
+
+; Table for converting field value metadata
+; NOTE: These take place after the field keys have changed
+(def ^{:private true} field-value-conversions
+  {:identifier str->boolean
+   :required str->boolean})
+
+(defn- convert-values
+  [field]
+  (reduce (fn [f [k v]]
+            (if (contains? f k) (update f k v) f))
+          field field-value-conversions))
+
+(defn process-field
+  [field]
+  (->
+    (clojure.set/rename-keys field field-key-changes)
+    (convert-values)
+    (process-validation)))
+
+(defn try-process-field
+  [field]
+  (try
+    (process-field field)
+    (catch Exception e
+      (prn field)
+      (throw e))))
---
+++ test/redshoe/field_test.clj
@@ -0,0 +1,195 @@
+(ns redshoe.field-test
+  (:require [clojure.test :refer :all]
+            [redshoe.field :refer :all]
+            [clj-time.core :as t]))
+
+(deftest renaming-keys
+  (is (= (process-field {:field_type "foo"}) {:type "foo"}))
+  (is (= (process-field {:field_name "foo"}) {:name "foo"}))
+  (is (= (process-field {:field_label "foo"}) {:label "foo"})))
+
+(deftest converting-values
+  (is (= (process-field {:identifier "y"}) {:identifier true}))
+  (is (= (process-field {:identifier "n"}) {:identifier false}))
+  (is (= (process-field {:identifier ""}) {:identifier false}))
+  (is (= (process-field {:identifier "junk"}) {:identifier false}))
+  (is (= (process-field {:required "y"}) {:required true}))
+  (is (= (process-field {:required "n"}) {:required false}))
+  (is (= (process-field {:required ""}) {:required false}))
+  (is (= (process-field {:required "junk"}) {:required false})))
+
+(def ^{:private true} text-validation-types
+  {:number {:text_validation_type_or_show_slider_number "number"}
+   :integer {:text_validation_type_or_show_slider_number "integer"}
+   :date-ymd {:text_validation_type_or_show_slider_number "date_ymd"}
+   :date-dmy {:text_validation_type_or_show_slider_number "date_dmy"}
+   :date-mdy {:text_validation_type_or_show_slider_number "date_mdy"}
+   :datetime-ymd {:text_validation_type_or_show_slider_number "datetime_ymd"}
+   :datetime-dmy {:text_validation_type_or_show_slider_number "datetime_dmy"}
+   :datetime-mdy {:text_validation_type_or_show_slider_number "datetime_mdy"}
+   :datetime-seconds-ymd {:text_validation_type_or_show_slider_number "datetime_seconds_ymd"}
+   :datetime-seconds-dmy {:text_validation_type_or_show_slider_number "datetime_seconds_dmy"}
+   :datetime-seconds-mdy {:text_validation_type_or_show_slider_number "datetime_seconds_mdy"}})
+
+(deftest processing-validation
+  (testing "text field type"
+    (let [field {:field_type "text"}]
+      (testing "number validation type"
+        (let [field (merge field (:number text-validation-types))]
+          (is (= (process-field field)
+              {:type "text" :validation {:type "number"}}))
+          (is (= (process-field (merge field {:text_validation_min "1.234"}))
+              {:type "text" :validation {:type "number" :min 1.234M}}))
+          (is (= (process-field (merge field {:text_validation_max "1.234"}))
+              {:type "text" :validation {:type "number" :max 1.234M}}))
+          (is (= (process-field
+                (merge field {:text_validation_min "1.234" :text_validation_max "2.345"}))
+              {:type "text" :validation {:type "number" :min 1.234M :max 2.345M}}))))
+
+      (testing "integer validation type"
+        (let [field (merge field (:integer text-validation-types))]
+          (is (= (process-field field)
+              {:type "text" :validation {:type "integer"}}))
+          (is (= (process-field (merge field {:text_validation_min "1234"}))
+              {:type "text" :validation {:type "integer" :min 1234}}))
+          (is (= (process-field (merge field {:text_validation_max "1234"}))
+              {:type "text" :validation {:type "integer" :max 1234}}))
+          (is (= (process-field
+                (merge field {:text_validation_min "1234" :text_validation_max "2345"}))
+              {:type "text" :validation {:type "integer" :min 1234 :max 2345}}))))
+
+      (testing "date_ymd validation type"
+        (let [field (merge field (:date-ymd text-validation-types))]
+          (is (= (process-field field)
+              {:type "text" :validation {:type "date_ymd"}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12"}))
+              {:type "text" :validation {:type "date_ymd" :min (t/date-time 2000 1 12)}}))
+          (is (= (process-field (merge field {:text_validation_max "2000-01-12"}))
+              {:type "text" :validation {:type "date_ymd" :max (t/date-time 2000 1 12)}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12"
+                                              :text_validation_max "2099-01-12"}))
+                 {:type "text"
+                  :validation {:type "date_ymd"
+                               :min (t/date-time 2000 1 12)
+                               :max (t/date-time 2099 1 12)}}))))
+
+      (testing "date_dmy validation type"
+        (let [field (merge field (:date-dmy text-validation-types))]
+          (is (= (process-field field)
+              {:type "text" :validation {:type "date_dmy"}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12"}))
+              {:type "text" :validation {:type "date_dmy" :min (t/date-time 2000 1 12)}}))
+          (is (= (process-field (merge field {:text_validation_max "2000-01-12"}))
+              {:type "text" :validation {:type "date_dmy" :max (t/date-time 2000 1 12)}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12"
+                                              :text_validation_max "2099-01-12"}))
+                 {:type "text"
+                  :validation {:type "date_dmy"
+                               :min (t/date-time 2000 1 12)
+                               :max (t/date-time 2099 1 12)}}))))
+
+      (testing "date_mdy validation type"
+        (let [field (merge field (:date-mdy text-validation-types))]
+          (is (= (process-field field)
+              {:type "text" :validation {:type "date_mdy"}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12"}))
+              {:type "text" :validation {:type "date_mdy" :min (t/date-time 2000 1 12)}}))
+          (is (= (process-field (merge field {:text_validation_max "2000-01-12"}))
+              {:type "text" :validation {:type "date_mdy" :max (t/date-time 2000 1 12)}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12"
+                                              :text_validation_max "2099-01-12"}))
+                 {:type "text"
+                  :validation {:type "date_mdy"
+                               :min (t/date-time 2000 1 12)
+                               :max (t/date-time 2099 1 12)}}))))
+
+      (testing "datetime_ymd validation type"
+        (let [field (merge field (:datetime-ymd text-validation-types))]
+          (is (= (process-field field)
+              {:type "text" :validation {:type "datetime_ymd"}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12 23:59"}))
+              {:type "text" :validation {:type "datetime_ymd" :min (t/date-time 2000 1 12 23 59)}}))
+          (is (= (process-field (merge field {:text_validation_max "2000-01-12 23:59"}))
+              {:type "text" :validation {:type "datetime_ymd" :max (t/date-time 2000 1 12 23 59)}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12 23:59"
+                                              :text_validation_max "2099-01-12 23:59"}))
+                 {:type "text"
+                  :validation {:type "datetime_ymd"
+                               :min (t/date-time 2000 1 12 23 59)
+                               :max (t/date-time 2099 1 12 23 59)}}))))
+
+      (testing "datetime_dmy validation type"
+        (let [field (merge field (:datetime-dmy text-validation-types))]
+          (is (= (process-field field)
+              {:type "text" :validation {:type "datetime_dmy"}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12 23:59"}))
+              {:type "text" :validation {:type "datetime_dmy" :min (t/date-time 2000 1 12 23 59)}}))
+          (is (= (process-field (merge field {:text_validation_max "2000-01-12 23:59"}))
+              {:type "text" :validation {:type "datetime_dmy" :max (t/date-time 2000 1 12 23 59)}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12 23:59"
+                                              :text_validation_max "2099-01-12 23:59"}))
+                 {:type "text"
+                  :validation {:type "datetime_dmy"
+                               :min (t/date-time 2000 1 12 23 59)
+                               :max (t/date-time 2099 1 12 23 59)}}))))
+
+      (testing "datetime_mdy validation type"
+        (let [field (merge field (:datetime-mdy text-validation-types))]
+          (is (= (process-field field)
+              {:type "text" :validation {:type "datetime_mdy"}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12 23:59"}))
+              {:type "text" :validation {:type "datetime_mdy" :min (t/date-time 2000 1 12 23 59)}}))
+          (is (= (process-field (merge field {:text_validation_max "2000-01-12 23:59"}))
+              {:type "text" :validation {:type "datetime_mdy" :max (t/date-time 2000 1 12 23 59)}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12 23:59"
+                                              :text_validation_max "2099-01-12 23:59"}))
+                 {:type "text"
+                  :validation {:type "datetime_mdy"
+                               :min (t/date-time 2000 1 12 23 59)
+                               :max (t/date-time 2099 1 12 23 59)}}))))
+
+      (testing "datetime_seconds_ymd validation type"
+        (let [field (merge field (:datetime-seconds-ymd text-validation-types))]
+          (is (= (process-field field)
+              {:type "text" :validation {:type "datetime_seconds_ymd"}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12 23:59:59"}))
+              {:type "text" :validation {:type "datetime_seconds_ymd" :min (t/date-time 2000 1 12 23 59 59)}}))
+          (is (= (process-field (merge field {:text_validation_max "2000-01-12 23:59:59"}))
+              {:type "text" :validation {:type "datetime_seconds_ymd" :max (t/date-time 2000 1 12 23 59 59)}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12 23:59:59"
+                                              :text_validation_max "2099-01-12 23:59:59"}))
+                 {:type "text"
+                  :validation {:type "datetime_seconds_ymd"
+                               :min (t/date-time 2000 1 12 23 59 59)
+                               :max (t/date-time 2099 1 12 23 59 59)}}))))
+
+      (testing "datetime_seconds_dmy validation type"
+        (let [field (merge field (:datetime-seconds-dmy text-validation-types))]
+          (is (= (process-field field)
+              {:type "text" :validation {:type "datetime_seconds_dmy"}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12 23:59:59"}))
+              {:type "text" :validation {:type "datetime_seconds_dmy" :min (t/date-time 2000 1 12 23 59 59)}}))
+          (is (= (process-field (merge field {:text_validation_max "2000-01-12 23:59:59"}))
+              {:type "text" :validation {:type "datetime_seconds_dmy" :max (t/date-time 2000 1 12 23 59 59)}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12 23:59:59"
+                                              :text_validation_max "2099-01-12 23:59:59"}))
+                 {:type "text"
+                  :validation {:type "datetime_seconds_dmy"
+                               :min (t/date-time 2000 1 12 23 59 59)
+                               :max (t/date-time 2099 1 12 23 59 59)}}))))
+
+      (testing "datetime_seconds_mdy validation type"
+        (let [field (merge field (:datetime-seconds-mdy text-validation-types))]
+          (is (= (process-field field)
+              {:type "text" :validation {:type "datetime_seconds_mdy"}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12 23:59:59"}))
+              {:type "text" :validation {:type "datetime_seconds_mdy" :min (t/date-time 2000 1 12 23 59 59)}}))
+          (is (= (process-field (merge field {:text_validation_max "2000-01-12 23:59:59"}))
+              {:type "text" :validation {:type "datetime_seconds_mdy" :max (t/date-time 2000 1 12 23 59 59)}}))
+          (is (= (process-field (merge field {:text_validation_min "2000-01-12 23:59:59"
+                                              :text_validation_max "2099-01-12 23:59:59"}))
+                 {:type "text"
+                  :validation {:type "datetime_seconds_mdy"
+                               :min (t/date-time 2000 1 12 23 59 59)
+                               :max (t/date-time 2099 1 12 23 59 59)}}))))
+      )))
