401761a87e70d5a963dccaa42ce34155a68a920a
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2017-02-02 16:53:22 -0600

    Use field/process-field in util namespace
    - remove field processing logic from util namespace
    - rename helper functions to indicate no processing is being done

--- src/redshoe/util.clj
+++ src/redshoe/util.clj
@@ -1,97 +1,25 @@
 (ns redshoe.util
   (:require [clojure.string :as string]
+            [redshoe.field :as field]
             [redshoe.http :as http]
-            [redshoe.xml :as xml]
-            clj-time.format))
+            [redshoe.xml :as xml]))
 
 ; Helper composition functions
-(def export-arms (comp xml/arms->seq http/export-arms))
-(def export-events (comp xml/events->seq http/export-events))
-(def export-mappings (comp xml/items->seq http/export-mappings))
-(def export-records (comp xml/records->seq http/export-records))
-(def export-field-names (comp xml/fields->seq http/export-field-names))
-
-(defn- parse-datetime
-  [fmt-str value]
-  (->
-    (clj-time.format/formatter-local fmt-str)
-    (clj-time.format/parse-local value)))
-
-(def ^{:private true} validation-coercions
-  {"date_ymd" (partial parse-datetime "yyyy-MM-dd")
-   "date_mdy" (partial parse-datetime "MM-dd-yyyy")
-   "date_dmy" (partial parse-datetime "dd-MM-yyyy")
-   "time" (partial parse-datetime "HH:mm")
-   "datetime_ymd" (partial parse-datetime "yyyy-MM-dd HH:mm")
-   "datetime_mdy" (partial parse-datetime "MM-dd-yyyy HH:mm")
-   "datetime_dmy" (partial parse-datetime "dd-MM-yyyy HH:mm")
-   "datetime_seconds_ymd" (partial parse-datetime "yyyy-MM-dd HH:mm:ss")
-   "datetime_seconds_mdy" (partial parse-datetime "MM-dd-yyyy HH:mm:ss")
-   "datetime_seconds_dmy" (partial parse-datetime "dd-MM-yyyy HH:mm:ss")
-   "number" bigdec
-   "integer" #(Integer. %)})
-
-(defn- process-field
-  [field]
-  (let [{ftype :field_type
-         vtype :text_validation_type_or_show_slider_number
-         vmin :text_validation_min
-         vmax :text_validation_max
-         choices :select_choices_or_calculations
-         identifier :identifier} field
-        field (->
-                (if (contains? field :identifier)
-                  (assoc field :identifier (= identifier "y"))
-                  field)
-                (dissoc
-                  :text_validation_type_or_show_slider_number
-                  :text_validation_min
-                  :text_validation_max
-                  :select_choices_or_calculations))]
-
-    (case ftype
-      "text"
-      (let [f (get validation-coercions vtype)]
-        (->>
-          (filter second
-                  {:type vtype
-                   :min (and f vmin (f vmin))
-                   :max (and f vmax (f vmax))
-                   })
-          (into {})
-          (assoc field :validation)))
-
-      "slider"
-      (->>
-        (filter second
-                {:show_slider_number vtype
-                 :slider_labels (string/split choices #"\s+\|\s+")})
-        (into {})
-        (merge field))
-
-      ("dropdown" "radio" "checkbox")
-      (->>
-        (string/split choices #"\s+\|\s+")
-        (mapv #(zipmap [:code :label] (string/split % #",\s+")))
-        (assoc field :choices))
-
-      "calc"
-      (assoc field :formula choices)
-
-      ; default
-      field)))
+(def raw-export-arms (comp xml/arms->seq http/export-arms))
+(def raw-export-events (comp xml/events->seq http/export-events))
+(def raw-export-mappings (comp xml/mappings->seq http/export-mappings))
+(def raw-export-records (comp xml/records->seq http/export-records))
+(def raw-export-field-names (comp xml/field-names->seq http/export-field-names))
+(def raw-export-fields (comp xml/fields->seq http/export-fields))
 
 (defn export-fields
   "Export field metadata from the REDCap API and process"
   [url token]
-  (->>
-    (http/export-fields url token)
-    (xml/records->seq)
-    (map process-field)))
+  (map field/try-process-field (raw-export-fields url token)))
 
 (defn- add-exports-to-field
   [field-name-groups field]
-  (assoc field :exports (get field-name-groups (:field_name field))))
+  (assoc field :exports (get field-name-groups (:name field))))
 
 (defn- organize-fields
   [fields field-name-groups]
@@ -120,11 +48,11 @@
 (defn export-arms-tree
   "Fetch field metadata and organize by arm and event"
   [url token]
-  (let [field-names (export-field-names url token)
+  (let [field-names (raw-export-field-names url token)
         fields (export-fields url token)
-        mappings (export-mappings url token)
-        events (export-events url token)
-        arms (export-arms url token)]
+        mappings (raw-export-mappings url token)
+        events (raw-export-events url token)
+        arms (raw-export-arms url token)]
     (->>
       (organize-fields fields (group-by :original_field_name field-names))
       (group-by :form_name)
@@ -145,5 +73,14 @@
    (if-not (empty? id-partitions)
      (let [ids (string/join "," (first id-partitions))]
        (lazy-cat
-         (export-records url token {:records ids})
+         (raw-export-records url token {:records ids})
          (export-chunked-records url token (rest id-partitions)))))))
+
+(defn export-records-tree
+  "Fetch records from the REDCap API and reorganize into a tree"
+  [url token primary-key]
+  (->>
+    (raw-export-records url token)
+    (group-by primary-key)
+    (map (fn [[id data]] [id (group-by :redcap_event_name data)]))
+    (into {})))
